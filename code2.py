2ae3bf27c640e5abc424affbb1cd2fad02b8a799
unknown <gdutt2304@gmail.com>
2026-03-28 16:55:36 +0530

    ASsigment operators

--- code2.py
+++ code2.py
@@ -12,7 +12,7 @@
 # power  =  2
 # print(power**base)
 # 2^12
-
+'''
 a = 5
 b = 5
 print(a == b)
@@ -21,3 +21,43 @@ print(a > b)
 print(a >= b)
 print(a <= b)
 print(a < b)
+'''
+
+# OR , AND , NOT
+# True and False = False
+# False and False = False
+# False and True = False
+# True and True = True
+
+
+
+# True OR False = True
+# False OR False = False
+# False Or True = True
+# True OR True = True
+#  not(True) =  False
+
+
+# a = 10
+# b = 5
+# c = 20
+
+# print(a >b and c > a)
+# print(a <b or c > a)
+# print(not(a>b))
+
+# x = 10
+# x+=5
+# x-=5
+# x*=5
+# x//=5
+# x/=5
+# x%=5
+# x**=5 z = x**5
+# x = 5 +  x
+# print(x)
+
+# a = 6
+# print(6)
+# a = 8
+# print(a)
