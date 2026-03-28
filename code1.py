5d191e491abca334c01ef3b5208f3436f5795a85
unknown <gdutt2304@gmail.com>
2026-03-28 16:24:58 +0530

    first commit

---
+++ code1.py
@@ -0,0 +1,37 @@
+# print("Hello world ")
+# a =  5 
+# b = 7
+# x  = "Python"
+# print(x)
+#  int  , float, str , bool
+
+# a = 6
+# print( type(a), a)
+# b  = 6.9
+# print( type(b), b)
+# x = "Python"
+# print( type(x), x)
+# y = False
+# print( type(y), y)
+
+# value = input("Enter your value : ")
+# print(value)
+
+
+num = str(25)
+print( type(num),  num)
+
+value  = int("25")
+print( type(value), value)
+
+a = float(5)
+print(type(a) , a)
+
+a =  bool(0)
+print(type(a), a)
+
+
+
+
+
+# comment = ctrl  + /
