05f63a4d94469361cf129bbc28b45391ea9d3d15
unknown <gdutt2304@gmail.com>
2026-03-29 19:04:46 +0530

    Quesitons

--- code3.py
+++ code3.py
@@ -25,12 +25,72 @@
 # else:
 #     print("Fail")
 
-num  = -5
+# num  = -5
 
-if num >= 0:
-    if num == 0:
-        print("Zero")
-    else:
-        print("Positive")
-else:
-    print("Negative")
+# if num >= 0:
+#     if num == 0:
+#         print("Zero")
+#     else:
+#         print("Positive")
+# else:
+#     print("Negative")
+
+# Questions 
+
+# age  = 5
+
+# if age <= 5:
+#     print("Ticket Free")
+# elif age >5 and age <=15:
+#     print("ticket price is 100")
+# elif age > 15 and age <= 60:
+#     print("Ticket price is 200")
+# else:
+#     print("ticket price is 50")     
+# 
+# 
+#  <!-- 1. Traffic Light System
+#  Raste par lagi traffic light kaise kaam karti hai, uska simple logic:
+#  Logic:
+#  Red -> Stop
+#  Yellow -> Wait/Ready
+#  Green -> Go -->
+
+# <!-- 2- Online Shopping Discount (Multiple slabs)
+# Ek e-commerce website ke liye logic likhiye jo total bill par discount calculate kare.
+
+# Logic:
+
+# Bill > ₹5000: 20% Discount
+
+# Bill ₹2000 se ₹5000: 10% Discount   
+# 
+# 
+# 
+# . The Login Authenticator (Nested if)
+# Scenario: Create a simple two-step verification script.
+
+# First, ask the user for a username.
+
+# If the username is "admin", ask for a password.
+
+# If the password is "python123", print "Access Granted."
+
+# Else (if the password is wrong), print "Wrong Password."
+
+# Else (if the username is not "admin"), print "Unknown User."    
+
+# Number Classifier (Combined)
+# Scenario: Write a program that takes an integer input and classifies it using both elif and nested if logic:
+
+# If the number is positive:
+
+# Check if it is even or odd (Nested).
+
+# If the number is negative:
+
+# Print "Negative Number."
+
+# If the number is zero:
+
+# Print "The number is Zero."
