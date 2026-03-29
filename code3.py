bf24a00e1b604ba488727400168d75604744149f
unknown <gdutt2304@gmail.com>
2026-03-29 18:33:52 +0530

    Control Flow Statements

---
+++ code3.py
@@ -0,0 +1,26 @@
+# a = 2
+# if(a%2==0):
+#     print("even")
+# a = 5
+# if(a%2==0):
+#     print("even")
+# else:
+#     print("odd")    
+
+# marks  = 80
+
+# if marks>=60:
+#     print("pass")
+# else:
+#     print("Fail")
+        
+
+marks  = 12
+if marks>=60:
+    print("grade A")
+elif marks >= 50:
+    print("grade B")
+elif marks >= 40:
+    print("grade C")
+else:
+    print("Fail")
