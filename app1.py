8c5bbded4d542f2ab961096c0c52667a228a73a3
unknown <gdutt2304@gmail.com>
2026-04-04 12:02:16 +0530

    basic

--- app1.py
+++ app1.py
@@ -1,13 +1,42 @@
-name  = "Jyoti"
-a = 123454321
-b = 1234321.34543
-c = True
-print(type(b) , b)
-print(type(a) , a)
-print(type(name) , name)
-print(type(c) , c)
+# name  = "Jyoti"
+# a = 123454321
+# b = 1234321.34543
+# c = True
+# print(type(b) , b)
+# print(type(a) , a)
+# print(type(name) , name)
+# print(type(c) , c)
+
+# name  =  input("Enter your name")
+# print(name)
+
+# name  = "20"
+# number  = 78
+# newFloat = float(number)
+# print(type(number) ,number)
+# print(type(newFloat) ,newFloat)
+# newNum  = str(number)
+# print(type(newNum))
+
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(1))
+# print(bool(0))
+# newName  = int(name)
+# print(type(name) , name )
+# print(type(newName) , newName )
 
 
 
-
+# ctrl + /  = comment 
 # String  = ""
