6425df1ab37b5660a6f93ca602c13b7c8c8c7a78
unknown <gdutt2304@gmail.com>
2026-04-05 11:54:08 +0530

    Conditional Statements

---
+++ app3.py
@@ -0,0 +1,24 @@
+# age  = 12
+
+# if  age >= 18:
+#     print("You are eligible to vote!")
+
+# num = 4
+
+# if num % 2 == 0 :
+#     print("Even")   #true 
+# else :
+#     print("odd")    #false 
+
+marks = 49
+
+if marks >= 90:
+    print("Grade : A")
+elif marks >= 70:
+    print("Grade : B")
+elif marks >= 60:
+    print("Grade : C")
+elif marks >= 50:
+    print("Grade : D")
+else:
+    print("Grade : F")    
