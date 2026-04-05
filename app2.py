d8d293d6fa0ae46006fcb571da048b2218511020
unknown <gdutt2304@gmail.com>
2026-04-05 11:10:43 +0530

    update operator

---
+++ app2.py
@@ -0,0 +1,46 @@
+# a = 5
+# b = 2
+
+# c  = a+b
+# c  = a-b
+# c  = a*b
+# c  = a/b
+# c  = a//b
+# c  = a%b
+# c  = a**b
+# print(c)
+
+
+# a = 6
+# b = 8
+
+# print(a  == b)
+# print(a  > b)
+# print(a  < b)
+# print(a  <= b)
+# print(a  >= b)
+# print(a  != b)
+
+# a = 10
+# b = 20 
+# c = 5
+# print(a < b  and a <c)
+# print(a < b  or a <c)
+# print(not(a < b))
+# AND ,  OR , NOT
+
+# true and true  = true
+# true and false  = false
+# false and true  = false
+# false and false  = false
+
+# true  or true  = true
+# true  or false  = true
+# false or  true  = true
+# false or  false  = false
+
+# not(true) = false  
+
+a  = 6
+a +=5             # a = a +5
+print(a)
