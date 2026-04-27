4185ad9ed698b7747ef45643c5312271b7b5bebe
unknown <gdutt2304@gmail.com>
2026-04-27 17:05:45 +0530

    set and dictionary

---
+++ app7.py
@@ -0,0 +1,14 @@
+# student = {
+#     "name":"Jyoti",
+#     "course":"python",
+#     "testMarks":80
+# }
+
+# print(student["name"])
+
+# student["city"] ="Noida"
+# print(student)
+
+# set 
+# my_Skills  = {"react","Python","JS","Java","python"}
+# print(my_Skills)
