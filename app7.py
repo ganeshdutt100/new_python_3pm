51e9f55d43ca7c6fc2400d7b4026c55ff8b9e9eb
unknown <gdutt2304@gmail.com>
2026-05-02 18:01:00 +0530

    Dictionary

--- app7.py
+++ app7.py
@@ -12,3 +12,12 @@
 # set 
 # my_Skills  = {"react","Python","JS","Java","python"}
 # print(my_Skills)
+
+fact = 1
+n = 4
+
+for i in range(1,n+1):
+    fact = fact *i
+print(fact)    
+
+# 4 = 4*3*2*1 = 120
---
+++ app8.py
@@ -0,0 +1,28 @@
+# name = "aman"
+# age = 20
+# course = "python"
+# key or values :
+# student ={
+#     "name":"Aman",
+#     "age":28,
+#     "course":"Python"
+# }
+
+# print(student["name"])
+# print(student["age"])
+
+menu_card = {
+    "Pizza":300,
+    "Burger":200,
+    "Cold Coffee":120
+}
+menu_card["French Fries"] = 100
+item = "French Fries"
+print(f"{item} ka price hai  :  {menu_card[item]}")
+
+
+
+
+# name  =  "Ganesh Dutt"
+# print( "Hello", name)
+# print(f"Hello {name} ")
