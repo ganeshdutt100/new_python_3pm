496a0b88922b9ffb957331caec33eebc7da14eed
unknown <gdutt2304@gmail.com>
2026-05-02 18:56:15 +0530

    Dictionary Questions

--- app8.py
+++ app8.py
@@ -11,18 +11,34 @@
 # print(student["name"])
 # print(student["age"])
 
-menu_card = {
-    "Pizza":300,
-    "Burger":200,
-    "Cold Coffee":120
-}
-menu_card["French Fries"] = 100
-item = "French Fries"
-print(f"{item} ka price hai  :  {menu_card[item]}")
+# menu_card = {
+#     "Pizza":300,
+#     "Burger":200,
+#     "Cold Coffee":120
+# }
+# menu_card["French Fries"] = 100
+# item = "French Fries"
+# for i in menu_card:
+#     print(f"{i} ka price hai  :  {menu_card[i]}")
 
 
+# search_name  =  "jyoti"    
+# print(search_name)
 
 
 # name  =  "Ganesh Dutt"
 # print( "Hello", name)
 # print(f"Hello {name} ")
+
+
+phonebook={
+    "a":"76545678987654",
+    "b":"234567654",
+    "c":"00000076545678987654",
+    "d":"87654376545678987654"
+}
+search_name =  "d"
+if  search_name in phonebook:
+    print(f"{search_name} ka number hai : {phonebook[search_name]}")
+else:
+    print(f"sorry")    
