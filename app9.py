a1cc7eb7891402a3799b25ee623898dc62d6ed4a
unknown <gdutt2304@gmail.com>
2026-05-03 17:47:57 +0530

    Udate list

---
+++ app9.py
@@ -0,0 +1,18 @@
+listValue  = ["Jyoti","Aman","Abhishek"]
+print(len(listValue))
+# print(listValue)
+# print(listValue[0])
+
+# a = []
+# a = ["apple","banana","milk"]
+# a = ["apple","banana",12,45,778]
+# a.append("python")
+# a.insert(0,"JavaScript")
+# a.remove("banana")
+
+# a[5]="Pyhton"
+# print(a.pop())
+# a=["jyoti","aman","abhishek"]
+# print(a.sort())
+# print(a)
+
