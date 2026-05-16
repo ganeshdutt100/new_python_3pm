c6510491c4b77c32e7418ac2a9e0f9ff4872265e
unknown <gdutt2304@gmail.com>
2026-05-16 19:06:18 +0530

    file dandling

--- app11.py
+++ app11.py
@@ -1,4 +1,15 @@
-def greet(name):
-    print(f"Hello {name}")
+# def greet(name):
+#     print(f"Hello {name}")
 
-greet("Krishna Dutt")    
+# greet("Krishna Dutt")    
+# def greet(a,b):
+#     return a-b
+
+# a = 5
+# b = 89
+# result = greet(a,b)
+# print(result)
+
+# lambdaFunction  = lambda a ,b: a +b 
+# lambdaFunction  = lambda name : name + "Dutt"
+# print(lambdaFunction("Ganesh "))
---
+++ app12.py
@@ -0,0 +1,16 @@
+# file  = open("aman.txt", "r")
+# data = file.read()   
+# print("file read successfully :  " , data) 
+# file.close()
+
+# file  = open("aman.txt", "a")
+# file.write("hey \n")
+# print("file created successfully") 
+# file.close() 
+
+
+# file  = open("aman.txt", "w")
+# file.write("hey \n")
+# print("file write successfully") 
+# file.close() 
+# 
