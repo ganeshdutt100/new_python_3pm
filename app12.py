72ac4746407b6fb6723fb2aa148a432bcc015b49
unknown <gdutt2304@gmail.com>
2026-05-17 18:22:27 +0530

    questions

--- app12.py
+++ app12.py
@@ -25,3 +25,29 @@
 # with open("hello.txt","r") as file:
 #     data = file.read()
 #     print(data)
+
+
+# num   = 12
+# if num > 0:
+#     print("Number is positive ")
+# elif num < 0:
+#     print("Number is negative")
+# else:
+#     print("Number is zero")    
+
+# a  = 5
+# b = 16
+# c = 8
+
+# if a >= b and a >= c:
+#     print("a is greater")
+# elif b >= a and b >= c:
+#     print("b is greater")
+# else:
+#     print("c is greater")
+
+# length  = 10
+# width = 2
+
+# area =length * width
+# print(area)
